1dc21ef29f3e1a3047c58b5811c8299c9d4bddc6
Pham Ich Bo <phamichbo13082003@gmail.com>
2025-02-28 19:04:51 +0700

    manage blog

--- server/src/api/v1/controllers/client/labrary.controller.js
+++ server/src/api/v1/controllers/client/labrary.controller.js
@@ -2,40 +2,40 @@ const Labrary = require("../../models/labrary");
 
 //[POST] api/v1/labrary/add-labrary
 module.exports.addLabrary = async (req, res) => {
-    const {quizId, userId} = req.body;
+    const { quizId, userId } = req.body;
 
     const userLabrary = await Labrary.findOne({
         userId: userId,
         deleted: false
     })
 
-    if(userLabrary) {
-        if(!userLabrary.quizId.includes(quizId)) {
+    if (userLabrary) {
+        if (!userLabrary.quizId.includes(quizId)) {
             await Labrary.updateOne({
                 userId: userId,
-            }, {$push:{quizId}})
+            }, { $push: { quizId } })
         }
-    }else{
-        const labrary = new Labrary({userId: userId, quizId:quizId})
+    } else {
+        const labrary = new Labrary({ userId: userId, quizId: quizId })
         await labrary.save()
     }
 
-    res.json({code: 200, message: "Add Labrary Successfully", });
+    res.json({ code: 200, message: "Add Labrary Successfully", });
 
 }
 
 //[POST] api/v1/labrary/get-all
 module.exports.getLabrary = async (req, res) => {
-    const {userId} = req.query;
+    const { userId } = req.query;
 
     const labrary = await Labrary.findOne({
         userId: userId,
         deleted: false
-    }).populate({path:"quizId", populate:"userId"})
+    }).populate({ path: "quizId", populate: "userId" })
 
     res.json({
-        code:200,
-        data:labrary,
+        code: 200,
+        data: labrary,
     })
 
 }
