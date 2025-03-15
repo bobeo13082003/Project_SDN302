7b9c86e8c265ab3b1f5291ce9df90b39c42bd20e
Pham Ich Bo <phamichbo13082003@gmail.com>
2025-03-15 17:15:36 +0700

    api delete library

--- server/src/api/v1/routes/client/labrary.route.js
+++ server/src/api/v1/routes/client/labrary.route.js
@@ -5,7 +5,6 @@ const authorization = require('../../../../middleware/authorization.middleware')
 
 router.post('/add-labrary', authorization.Authorization, controller.addLabrary)
 router.get('/get-all', authorization.Authorization, controller.getLabrary)
-
-
+router.delete('/delete-labrary/:quizId', authorization.Authorization, controller.deleteLibrary)
 
 module.exports = router;
